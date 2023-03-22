b7296a45ccf91412c3ef27c1074986e0032dd4c8
Victor Izquierdo <vhmizq@gmail.com>
2023-03-21 22:15:50 -0600

    add table

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -7,28 +7,38 @@ import { Cart, CartItem } from '../models/cart.model';
   styles: [],
 })
 export class CartComponent implements OnInit {
-  cart: Cart = {items: [{
-    product: 'https://via.placeholder.com/150',
-    name: 'snickers',
-    price: 150,
-    quantity: 1,
-    id: 1
-  }]}
+  cart: Cart = {
+    items: [
+      {
+        product: 'https://via.placeholder.com/150',
+        name: 'snickers',
+        price: 150,
+        quantity: 1,
+        id: 1,
+      },
+    ],
+  };
 
   dataSource: Array<CartItem> = [];
-  
-   displayedColumns: Array<string> = [
+
+  displayedColumns: Array<string> = [
     'product',
     'name',
     'price',
     'quantity',
     'total',
-    'action'
-  ]
-  
-  constructor() { }
+    'action',
+  ];
+
+  constructor() {}
 
   ngOnInit(): void {
     this.dataSource = this.cart.items;
+  }
+
+  getTotal(items: Array<CartItem>): number {
+    return items
+      .map((item) => item.price * item.quantity)
+      .reduce((prev, current) => prev + current, 0)
   }
 }
